b9d2666e3c0b0f312dec538e9ef2105b03114e35
Cassandra Curcio <cassandra.curcio@gmail.com>
2023-07-02 11:54:51 -0400

    move single recipe to recipe-item component, used ngFor to render multiple recipe-items in recipe-list

--- src/app/components/recipes/recipe-list/recipe-list.component.ts
+++ src/app/components/recipes/recipe-list/recipe-list.component.ts
@@ -8,15 +8,21 @@ import { Recipe } from 'src/app/models/recipe.model';
 })
 export class RecipeListComponent {
   recipes: Recipe[] = [
-    new Recipe(
-      'A Test Recipe',
-      'This is simply a test',
-      'https://cdn.pixabay.com/photo/2017/11/16/18/51/kagyana-2955466_640.jpg'
-    ),
     // new Recipe(
-    //   'Another Test Recipe',
-    //   'This is simply another test',
-    //   'https://cdn.pixabay.com/photo/2015/10/26/07/21/vegetables-1006694_640.jpg'
+    {
+      name: 'A Test Recipe',
+      description: 'This is simply a test',
+      imagePath:
+        'https://cdn.pixabay.com/photo/2017/11/16/18/51/kagyana-2955466_640.jpg',
+      // ),
+    },
+    // new Recipe(
+    {
+      name: 'Another Test Recipe',
+      description: 'This is simply another test',
+      imagePath:
+        'https://cdn.pixabay.com/photo/2015/10/26/07/21/vegetables-1006694_640.jpg',
+    },
     // ),
   ];
 }
