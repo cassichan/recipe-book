d1ebd8a6b23362aa50c20dd36c4cbfe3a14a17b7
Cassandra Curcio <cassandra.curcio@gmail.com>
2023-09-06 13:54:03 -0400

    pass event, trigger when selects nav item, render recipes or shopping list

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,4 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  activeFeature = "recipes";
+
+  updateFeature(feature: string) {
+    this.activeFeature = feature;
+  }
 }
